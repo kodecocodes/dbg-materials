b1034087d92418dbab8fcfcaf94c29d8e7d19791
Matt Galloway <matt@galloway.me.uk>
2023-04-28 20:05:35 +0100

    Remove change that shouldn't have been in here

--- 26-sb-examples-improved-lookup/projects/starter/Allocator/Allocator/DSObjectiveCObject.m
+++ 26-sb-examples-improved-lookup/projects/starter/Allocator/Allocator/DSObjectiveCObject.m
@@ -37,9 +37,6 @@
     self.eyeColor = [UIColor brownColor];
     self.firstName = @"Derek";
     self.lastName = @"Selander";
-
-    NSString *name = @"Zoltan";
-    NSLog(@"Hello world, I am %@. I’m %d, and I live in %@.", name, 30, @"my father’s basement");
   }
   
   return self;
